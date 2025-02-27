18805cd4066414805a20c75fd99d69222fe7f503
Félix Delgadillo Barrios <38565840+FelixDeBa@users.noreply.github.com>
2025-02-27 13:17:23 -0600

    Added find_user and UserNotFound
    Added Find_user function and UserNotFound Custom Exception

--- get_ad_user.py
+++ get_ad_user.py
@@ -1,17 +1,23 @@
 import subprocess
 
+class UserNotFound(Exception):
+    def __init__(self,msg=None):
+        self.msg = msg
+        print("Error: User Not Found")
+        
 
-def execute_command(columns:str="", properties:str="", search_base:str="", ad_filter:str="*", out_format:str="csv"):
+def execute_command(columns:str="", properties:str="", search_base:str="", ad_filter:str="", out_format:str="csv", user:str=""):
     if out_format != "csv" and out_format != "":
         raise EOFError("Por el momento solo aceptamos formato CSV o sin formato")
     
     columns = f" | Select {columns}" if not(columns is None or columns == "") else ""
-    ad_filter = "*" if ad_filter == "" or ad_filter is None else ad_filter
+    ad_filter = f" -Filter {ad_filter}" if not(ad_filter == "" or ad_filter is None) else ""
     out_format = " | ConvertTo-" + out_format if not(out_format is None or out_format == "") else "| ConvertTo-Csv"
     properties = f" -Properties \\\"{properties}\\\"" if not (properties is None or properties == "") else ""
     search_base = f" -SearchBase \\\"{search_base}\\\"" if not(search_base is None or search_base == "") else ""
+    user = f" -Identity {user}" if not(user is None or user == "") else ""
     
-    command = f"%SystemRoot%\\system32\\WindowsPowershell\\v1.0\\powershell.exe -command \"Get-AdUser -Filter {ad_filter}{properties}{search_base}{columns}{out_format}\""
+    command = f"%SystemRoot%\\system32\\WindowsPowershell\\v1.0\\powershell.exe -command \"Get-AdUser{user}{ad_filter}{properties}{search_base}{columns}{out_format}\""
     
     # print(command)
     proc = subprocess.Popen(command, stdout=subprocess.PIPE, shell=True)
@@ -22,10 +28,17 @@ def execute_command(columns:str="", properties:str="", search_base:str="", ad_fi
     else:
         print(err)
 
+def find_user(user:str, columns:str="SamAccountName,Name,UserPrincipalName,Enabled"):
+    found_user = execute_command(user=user, columns=columns)
+    if found_user == []:
+        raise UserNotFound(f"User {user} Not Found")
+    else:
+        return found_user
+    
 
 def ad_users(columns:str=""):
-    users = execute_command()
-    return usuarios    
+    users = execute_command(ad_filter="*")
+    return users    
 
 def enabled_users(columns:str="SamAccountName,Name,UserPrincipalName,Enabled", out_format:str="csv"):
     enabled_filter = "{Enabled -eq $True}"
@@ -41,9 +54,9 @@ def disabled_users(columns:str="SamAccountName,Name,UserPrincipalName,Enabled", 
         
 
 #para usuarios en cuarentena la ou debe ser Cuarentena y el dc es DC=cerrey,dc=com,dc=mx
-def filter_by_ou(ou:str, dc:str, columns:str="SamAccountName,Name,UserPrincipalName,Enabled", out_format:str="csv"):
+def filter_by_ou(ou:str, dc:str, columns:str="SamAccountName,Name,UserPrincipalName,Enabled", out_format:str="csv", ad_filter:str="*"):
         
-    usuarios = execute_command(properties="LockedOut", search_base=f"OU={ou},{dc}", columns=columns)
+    usuarios = execute_command(properties="LockedOut", search_base=f"OU={ou},{dc}", columns=columns, ad_filter=ad_filter)
     return usuarios
         
         
